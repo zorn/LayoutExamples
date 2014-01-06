db95c5ba824d27540cc273e2c6b982414dc0bc58
Mike Zornek <mike@clickablebliss.com>
2014-01-06 15:10:32 -0500

    initial commit

---
+++ LayoutExamples/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LayoutExamples
+//
+//  Created by Michael Zornek on 1/6/14.
+//  Copyright (c) 2014 Michael Zornek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LayoutExamplesTests/LayoutExamplesTests.m
@@ -0,0 +1,34 @@
+//
+//  LayoutExamplesTests.m
+//  LayoutExamplesTests
+//
+//  Created by Michael Zornek on 1/6/14.
+//  Copyright (c) 2014 Michael Zornek. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LayoutExamplesTests : XCTestCase
+
+@end
+
+@implementation LayoutExamplesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
